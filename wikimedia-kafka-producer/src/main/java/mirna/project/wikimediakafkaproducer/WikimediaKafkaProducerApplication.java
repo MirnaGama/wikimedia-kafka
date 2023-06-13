88410b8c77eb4854c9d74cecb4a437440b0782d2
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-06-12 23:22:31 -0300

    Create module wikimedia-kafka-producer

---
+++ wikimedia-kafka-producer/src/main/java/mirna/project/wikimediakafkaproducer/WikimediaKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package mirna.project.wikimediakafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikimediaKafkaProducerApplication.class, args);
+	}
+
+}
