a5c7df0d70c8d98eac9449452d81e5fab4f46d1e
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-06-12 23:25:10 -0300

    Add service class with event handler

---
+++ wikimedia-kafka-producer/src/main/java/mirna/project/wikimediakafkaproducer/service/WikimediaChangesProducerService.java
@@ -0,0 +1,44 @@
+package mirna.project.wikimediakafkaproducer.service;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+
+import mirna.project.wikimediakafkaproducer.constants.TopicNameConstants;
+import mirna.project.wikimediakafkaproducer.constants.URLConstants;
+import mirna.project.wikimediakafkaproducer.handler.WikimediaChangesHandler;
+
+@Service
+public class WikimediaChangesProducerService {
+
+	private static final Logger _log = LoggerFactory.getLogger(WikimediaChangesProducerService.class);
+	private KafkaTemplate<String, String> kafkaTemplate;
+	
+	public WikimediaChangesProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+		this.kafkaTemplate = kafkaTemplate;
+	}
+	
+	public void sendMessage() throws InterruptedException {
+		
+		String topic = TopicNameConstants.TOPIC_WIKIMEDIA_RECENTCHANGE;
+		
+		// to read real time stream data from wikimedia
+		EventHandler eventHandler = new WikimediaChangesHandler(
+				kafkaTemplate, TopicNameConstants.TOPIC_WIKIMEDIA_RECENTCHANGE);
+		
+		EventSource.Builder builder = new EventSource.Builder(
+				eventHandler, URI.create(URLConstants.URL_WIKIMEDIA_RECENTCHANGE));
+		
+		EventSource eventSource = builder.build();
+		eventSource.start();
+		
+		TimeUnit.MINUTES.sleep(10);	
+	}
+}
